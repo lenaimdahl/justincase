248133ac2db2f3ecfaaa544dcd2485e5cf1f73b5
Florian Imdahl <git@ffflorian.de>
2026-03-29 21:04:18 +0200

    refactor(backend): Use frontend URL instead of production URL for CORS

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -3,19 +3,21 @@ import {Logger, ValidationPipe} from '@nestjs/common';
 import {AppModule} from './app.module';
 
 const isProduction = process.env.NODE_ENV !== 'development';
-const productionUrl = process.env.PROD_URL || '';
+const frontendURL = process.env.FRONTEND_URL || '';
 const logger = new Logger('main');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: isProduction ? {origin: productionUrl} : false,
-  });
+  const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({whitelist: true}));
+  app.enableCors({
+    credentials: true,
+    origin: isProduction ? frontendURL : true,
+  });
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
   logger.debug(`Server started on port ${process.env.PORT ?? 3000}`);
   if (isProduction) {
-    logger.debug(`CORS enabled for origin: ${productionUrl}`);
+    logger.debug(`CORS enabled for origin: ${frontendURL}`);
   } else {
     logger.debug('CORS disabled for development');
   }
