b0d8ce1daf40308af484c8a736eade993e716cbc
Florian Imdahl <git@ffflorian.de>
2026-03-29 20:52:14 +0200

    chore(backend): Log CORS status

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,9 +1,10 @@
 import {NestFactory} from '@nestjs/core';
-import {ValidationPipe} from '@nestjs/common';
+import {Logger, ValidationPipe} from '@nestjs/common';
 import {AppModule} from './app.module';
 
 const isProduction = process.env.NODE_ENV !== 'development';
 const productionUrl = process.env.RENDER_EXTERNAL_URL || process.env.PROD_URL || '';
+const logger = new Logger('main');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -12,5 +13,11 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({whitelist: true}));
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  logger.debug(`Server started on port ${process.env.PORT ?? 3000}`);
+  if (isProduction) {
+    logger.debug(`CORS enabled for origin: ${productionUrl}`);
+  } else {
+    logger.debug('CORS disabled for development');
+  }
 }
 bootstrap();
