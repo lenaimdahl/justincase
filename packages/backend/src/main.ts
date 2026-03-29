a8a0e4089d8d0e823bed0d59a482a003935d0bb5
Florian Imdahl <git@ffflorian.de>
2026-03-29 20:49:49 +0200

    fix(backend): Enable CORS

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -2,8 +2,13 @@ import {NestFactory} from '@nestjs/core';
 import {ValidationPipe} from '@nestjs/common';
 import {AppModule} from './app.module';
 
+const isProduction = process.env.NODE_ENV !== 'development';
+const productionUrl = process.env.RENDER_EXTERNAL_URL || process.env.PROD_URL || '';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: isProduction ? {origin: productionUrl} : false,
+  });
   app.useGlobalPipes(new ValidationPipe({whitelist: true}));
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
